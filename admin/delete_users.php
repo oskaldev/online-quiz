618b97ad12f4928e092ef740d5a5ebee8843ada3
oskal314 <artem-osipovartem3002@mail.ru>
2023-04-04 03:23:03 +0300

    update delete

--- admin/delete_users.php
+++ admin/delete_users.php
@@ -1,3 +1,24 @@
+<!-- <?php
+      // session_start();
+      // if (!isset($_SESSION["admin"])) {
+      // 
+      ?>
+  <script>
+    window.location = "index.php";
+  </script>
+ <?php
+  // }
+  // require_once "auth.php";
+  // require_once "../connection.php";
+  // $id = $_GET["id"];
+  // mysqli_query($link, "delete from registration where id=$id");
+  // 
+  ?>
+<script>
+  window.location.href = "users.php";
+</script>/ -->
+
+
 <?php
 session_start();
 if (!isset($_SESSION["admin"])) {
@@ -10,7 +31,19 @@ if (!isset($_SESSION["admin"])) {
 require_once "auth.php";
 require_once "../connection.php";
 $id = $_GET["id"];
-mysqli_query($link, "delete from registration where id=$id");
+
+mysqli_query($link, "select * from registration where username='$_POST[username]'");
+
+if (isset($_SESSION["username"]) and $_SESSION["username"] === $_POST["username"]) {
+  // если пользователь авторизован, то удаляем сессию
+  session_unset();
+  session_destroy();
+  header("Location: index.php");
+  exit();
+}
+mysqli_query($link, "DELETE FROM registration WHERE id='$id' ");
+
+
 ?>
 <script>
   window.location.href = "users.php";
