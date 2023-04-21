3e5ff56819deaff73b3bc0afb4b033068a8de48b
oskal314 <artem-osipovartem3002@mail.ru>
2023-04-21 17:39:48 +0300

    update

--- user/header.php
+++ user/header.php
@@ -23,7 +23,7 @@ require_once "../connection.php";
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="#">
+  <link rel="icon" type="image/x-icon" href="img/logo.png">
   <meta name="description" content="Тесты по английскому языку">
   <title>Тесты по английскому языку</title>
 
